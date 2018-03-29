4b49244c08858a1401331e14e3e760e31319be3d
Max Cho <chomax1023@gmail.com>
2018-03-29 16:18:23 +0800

    以處理字串陣列的方式完成日立的撰寫

--- calendar.py
+++ calendar.py
@@ -1,73 +1,62 @@
 """
+根據今天的日期顯示這個月的月曆
+"""
+import datetime
+"""
+import datetime
 datetime.date.today()
 datetime.date(2018,3,27)
 datetime.date.today().year
 datetime.date.today().month
 datetime.date.today().day
-"""
-import datetime
-
-Mon=["Mon"]
-Tue=["Tue"]
-Wed=["Wed"]
-Thu=["Thu"]
-Fri=["Fri"]
-Sat=["Sat"]
-Sun=["Sun"]
-def InputAry(dayNum,WeekDayNum):
-     
-    if WeekDayNum==0:
-        Mon.append(str(dayNum).rjust(3))
-    elif WeekDayNum==1:
-        Tue.append(str(dayNum).rjust(3))
-    elif WeekDayNum==2:
-        Wed.append(str(dayNum).rjust(3))
-    elif WeekDayNum==3:
-        Thu.append(str(dayNum).rjust(3))
-    elif WeekDayNum==4:
-        Fri.append(str(dayNum).rjust(3))
-    elif WeekDayNum==5:
-        Sat.append(str(dayNum).rjust(3))
-    else:
-        Sun.append(str(dayNum).rjust(3))
-
-    if dayNum==1:
-        for i in range(WeekDayNum):
-            InputAry(" ",i)  
-        
 begin=datetime.date(2018,3,1)
 beginweekday=begin.weekday()
-
-InputAry(begin.day,begin.weekday())
 delta=datetime.timedelta(days=1)
-begin+delta
-for i in range(30):
-    begin +=delta
-    InputAry(begin.day,begin.weekday())
-
 d1=iter(Mon)
-d2=iter(Tue)
-d3=iter(Wed)
-d4=iter(Thu)
-d5=iter(Fri)
-d6=iter(Sat)
-d7=iter(Sun)
+print(next(d1))
+"""
 
 
-print(next(d1)+" "+next(d2)+" "+next(d3)+" "+next(d4)+" "+next(d5)+" "+next(d6)+" "+next(d7))
-print(next(d1)+" "+next(d2)+" "+next(d3)+" "+next(d4)+" "+next(d5)+" "+next(d6)+" "+next(d7))
-print(next(d1)+" "+next(d2)+" "+next(d3)+" "+next(d4)+" "+next(d5)+" "+next(d6)+" "+next(d7))
-print(next(d1)+" "+next(d2)+" "+next(d3)+" "+next(d4)+" "+next(d5)+" "+next(d6)+" "+next(d7))
-print(next(d1)+" "+next(d2)+" "+next(d3)+" "+next(d4)+" "+next(d5)+" "+next(d6)+" "+next(d7))
-"""print(next(d1)+" "+next(d2)+" "+next(d3)+" "+next(d4)+" "+next(d5)+" "+next(d6)+" "+next(d7))"""
+'透過今天取得當月第一天與最後一天'
+
+today=datetime.date.today()
+
+'取得當月第一天'
+firstDay=datetime.date(today.year,today.month,1)
+
+'由當月第一天推算次月第一天'
+if (firstDay.month+1) > 12 :
+    NextMonth=(firstDay.month+1)%12
+    NextYear=today.year+((firstDay.month+1)//12)
+    LastDay=datetime.date(NextYear,NextMonth,1)
+else:
+    NextMonth=(today.month+1)%13
+    NextYear=today.year
+    LastDay=datetime.date(NextYear,NextMonth,1)
+
+'取得當月天數'
+DayNum=(LastDay-firstDay).days
+DayAry=range(1,DayNum+1)
 
 
+'確認當月第一天的weekday'
+firstDayWeekDay=firstDay.weekday()
+NoInMonth=""
 
-
-
-
-
-
+'顯示月曆'
+print(today.year,today.month,"的月曆")
+print("Mon Tur Wed Tru Fri Sat Sun")
+dayitem=iter(DayAry)
+wc=1
+for i in range(0,DayNum+firstDayWeekDay):
+    if i < firstDayWeekDay:
+        print(str(NoInMonth).rjust(3)+" ",end="")
+    else:
+        if wc%7>0: 
+            print(str(next(dayitem)).rjust(3)+" ",end="")
+        else:
+            print(str(next(dayitem)).rjust(3)+"\n")            
+    wc +=1
 
 
 
