bceadf6fb3f24de03dd7cd3906f6a2cbf210be99
minghungcho <ming@winmaster.com>
2018-04-08 23:20:59 -0400

    Self-exercise 2

--- MingEmployee.py
+++ MingEmployee.py
@@ -13,7 +13,6 @@ class Employee:
         "This function is called by repr(). It returns string representation to construct an employee."
         return f"Employee({self.id}, '{self.name}')"
 
-
 class EmployeeRepository:
     "EmployeeRepository provides a collection-like interface for accessing domain objects."
 
@@ -29,6 +28,13 @@ class EmployeeRepository:
         "To support -= for removing an employee from list."
         self._employees.remove(employee)
         return self
+
+    def add_employee(self, employee):
+        "Add new employee if employee id is new. Otherwise, throw ValueError"
+        if self.get_employee_by_id(employee.id) == None:
+            self._employees.append(employee)
+        else:
+            raise ValueError("Duplicated employee")
         
     def find(self, employee):
         "This function finds the specified employee in Employee repository. If found, it returns the employee object. Otherwise, returns None."
@@ -39,6 +45,13 @@ class EmployeeRepository:
 
     def get_employees_by_name(self, name=""):
         "Return any employee whose name contains specified name."
+
+        # result = []
+        # for emp in self._employees:
+        #     if name=="" or name.lower()==emp.name.lower():
+        #         result.append(emp)
+        # return result
+
         if len(self._employees)==0:
             raise StopIteration
 
